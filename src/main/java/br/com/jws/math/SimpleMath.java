23dbf98669e5da7f3c4c0bae8121576ecb1a482a
Yan Burigo <yanburigoribeiro@gmail.com>
2023-11-15 17:23:46 -0300

    Create others math operation

---
+++ src/main/java/br/com/jws/math/SimpleMath.java
@@ -0,0 +1,28 @@
+package br.com.jws.math;
+
+public class SimpleMath {
+
+	public Double sum(Double firstNumber, Double secondNumber) {
+		return firstNumber + secondNumber;
+	}
+	
+	public Double subtration(Double firstNumber, Double secondNumber) {
+		return firstNumber - secondNumber;
+	}
+	
+	public Double multiplication(Double firstNumber, Double secondNumber) {
+		return firstNumber * secondNumber;
+	}
+	
+	public Double division(Double firstNumber, Double secondNumber) {
+		return firstNumber / secondNumber;
+	}
+	
+	public Double mean(Double firstNumber, Double secondNumber) {
+		return (firstNumber + secondNumber) / 2;
+	}
+
+	public Double squareRoot(Double number) {
+		return Math.sqrt(number);
+	}
+}
